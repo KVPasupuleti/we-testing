20291c1959d95cc1433b728c404aa1bf5d27cf8c
KV_Pasupuleti <krishna.pasupuleti@nxtwave.co.in>
2022-12-03 10:47:11 +0530

    Remove notification click event listener in the Service Worker File

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -12,37 +12,6 @@ if ('function' === typeof importScripts) {
          window.location.reload()
       })
 
-      self.addEventListener('notificationclick', function (event) {
-         // Android doesn't close the notification when you click on it
-         // See: http://crbug.com/463146
-         event.notification.close()
-
-         // This looks to see if the current is already open and
-         // focuses if it is
-         event.waitUntil(
-            clients
-               .matchAll({
-                  type: 'window'
-               })
-               .then(function (clientList) {
-                  const additionalData = event?.notification?.data?.data
-                     ? JSON.parse(event.notification.data.data)
-                     : {}
-                  const url = event?.notification?.data?.url
-                  for (var i = 0; i < clientList.length; i++) {
-                     var client = clientList[i]
-                     if (client.url === url) return client.focus()
-                  }
-                  if (clients.openWindow) {
-                     if (url) {
-                        return clients.openWindow(url)
-                     }
-                     return clients.openWindow('/')
-                  }
-               })
-         )
-      })
-
       const manifest = self.__WB_MANIFEST
 
       workbox.precaching.precacheAndRoute([])
